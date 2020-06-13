e481c22965abab7ddcdf027bdf397212acb27ad2
v9364 <zw9364934@163.com>
2020-06-13 15:21:55 +0800

    第一次向GitHub传送

---
+++ demo01.py
@@ -0,0 +1,37 @@
+"""
+print("你好  hello world")
+print(())   #元组
+print([])   #数组
+print({})  #字典  
+print(True)  #布尔值
+
+'''
+  sdsadsdsa
+sdasd
+sdasd
+'''
+
+print("haha哈哈",23333)
+print("哈哈"+"嘻嘻")  #字符串的拼接,需要同样的字符类型
+# print("哈哈"*100)
+print((1+2-99*0.5)/3)
+
+#变量
+#赋值
+
+a=input("请输入：")   #input获取的默认为字符串
+b=input("请输入：")
+print("input获取的内容：",a+b)
+"""
+
+# #数据格式的转换
+# print(type(2333))   #显示数据的格式
+
+# a=int(("2333"))
+# print(type(a))
+
+#练习
+a=input("输入字符串1：")
+b=input("输入字符串2：")
+c=len(a)+len(b)
+print("输入字符串总长为：",c)
